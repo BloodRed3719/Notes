4590734df150d08bc03f5123764b3f97647cbe31
BloodRed3719 <138595384+BloodRed3719@users.noreply.github.com>
2023-07-07 21:52:07 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+app = Flask(__name__)
+
+# Initialize Firebase
+cred = credentials.Certificate("firebase-key.json")  # Replace with your own Firebase service account key
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://your-firebase-project.firebaseio.com/'
+})
+
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+@app.route('/store', methods=['POST'])
+def store():
+    value = request.form['value']
+
+    # Save the value to Firebase Realtime Database
+    ref = db.reference('/values')
+    ref.push(value)
+
+    return 'Value stored successfully!'
+
+if __name__ == '__main__':
+    app.run()
